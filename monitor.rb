7de7d84462df4796452cd6ebdede8945bf5554db
Vladimir Zhilyaev <vladimir@zhilyaev.kyiv.ua>
2017-12-27 17:32:38 +0200

    New structure using gem 'daemons'

--- monitor.rb
+++ monitor.rb
@@ -19,23 +19,17 @@ end
 
 def send_alert_mail(text_uri, old_state, new_state)
 
-  # puts "Mail sent: #{old_state} changed to #{new_state}"
-
   Mail.deliver do
-    from    '@gmail.com'
-    to      '@gmail.com'
-    subject 'Some of monitored sites changed their state'
-    body    "URI #{text_uri} changed state from #{old_state} to #{new_state}"
+    from    'donotreply'
+    to      'someone@example.com'
+    subject 'Monitored resource has changed its state'
+    body    "Resource #{text_uri} changed state from #{old_state} to #{new_state}"
   end
 end
 
 last_states = Hash.new("200:OK")
 
-Process.daemon
 while true do 
-  # puts "-"*30
-  # puts Time.now.strftime('%Y-%m-%d %H:%M:%S')
-  # puts "-"*30
   URI_TO_CHECK.each do |text_uri|
     uri = URI(text_uri)
     response = nil
@@ -49,7 +43,6 @@ while true do
       new_state =  e.to_s
     end
 
-    # puts "#{uri.host} => #{new_state}"
     send_alert_mail(text_uri, last_states[text_uri], new_state) if last_states[text_uri] != new_state
     last_states[text_uri] = new_state
   end
